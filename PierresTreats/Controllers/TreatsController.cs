976355dbeae7d5ea4e30c03c80e7fffe1dc2fe0c
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-01-14 13:23:27 -0900

    add join table functionality

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -24,9 +24,9 @@ namespace PierresTreats.Controllers
       return View();
     }
     [HttpPost]
-    public ActionResult Create(Treat Treat)
+    public ActionResult Create(Treat treat)
     {
-      _db.Treats.Add(Treat);
+      _db.Treats.Add(treat);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
@@ -40,13 +40,13 @@ namespace PierresTreats.Controllers
     }
     public ActionResult Edit(int id)
     {
-      var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
       return View(thisTreat);
     }
     [HttpPost]
-    public ActionResult Edit(Treat Treat)
+    public ActionResult Edit(Treat treat)
     {
-      _db.Entry(Treat).State = EntityState.Modified;
+      _db.Entry(treat).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
@@ -64,6 +64,34 @@ namespace PierresTreats.Controllers
         _db.Treats.Remove(thisTreat);
         _db.SaveChanges();
         return RedirectToAction("Index");
+    }
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
+      var listOfFlavors = new SelectList(_db.Flavors, "FlavorId", "Name");
+      if(listOfFlavors.Count() > 0)
+      {
+        ViewBag.FlavorId = listOfFlavors;
+      }
+      return View(thisTreat);
+    }
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int flavorId)
+    {
+      if(flavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = flavorId, TreatId = treat.TreatId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Details", new {id = treat.TreatId});
+    }
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(join => join.FlavorTreatId == joinId);
+      int treatId = joinEntry.TreatId;
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new {id = treatId});
     }
   }
 }
