13c94b5c4cab3858465f44a9195422dfc3edbb7d
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-01-14 10:44:17 -0900

    add details

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -19,5 +19,24 @@ namespace PierresTreats.Controllers
     {
       return View();
     }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Treat Treat)
+    {
+      _db.Treats.Add(Treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treats
+        .Include(Treat => Treat.JoinEntities)
+        .ThenInclude(join => join.Treat)
+        .FirstOrDefault(Treat => Treat.TreatId == id);
+      return View(thisTreat);
+    }
   }
 }
