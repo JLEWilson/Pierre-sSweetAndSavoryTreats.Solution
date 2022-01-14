d8777a30263da4b6725055a8c39edc6f1d688d92
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-01-14 08:31:26 -0900

    add controllers with index view

---
+++ PierresTreats/Controllers/FlavorController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PierresTreats.Controllers 
+{
+  public class FlavorsController : Controller
+  {
+    public readonly PierresTreatsContext _db;
+    public FlavorsController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ PierresTreats/Controllers/TreatsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PierresTreats.Controllers 
+{
+  public class TreatsController : Controller
+  {
+    public readonly PierresTreatsContext _db;
+    public TreatsController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
