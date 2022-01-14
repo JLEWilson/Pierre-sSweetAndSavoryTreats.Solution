df0bda583aa0ff07a2592cc7d49f630f997df314
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-01-14 08:18:21 -0900

    add objects and dbsets

--- PierresTreats/Models/PierresTreatsContex.cs
+++ PierresTreats/Models/PierresTreatsContex.cs
@@ -5,6 +5,9 @@ namespace PierresTreats.Models
 {
   public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
   {
+    DbSet<Flavor> Flavors { get; set; }
+    DbSet<Treat> Treats { get; set; }
+    DbSet<FlavorTreat> FlavorTreat { get; set; }
     public PierresTreatsContext(DbContextOptions options) : base(options){}
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
