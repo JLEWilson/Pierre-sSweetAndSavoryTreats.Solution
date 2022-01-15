eba8d8549a66c0dd7f49dd47b480bec3ddffdd62
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-01-14 15:38:18 -0900

    remove unecessary using directives

--- PierresTreats/Controllers/FlavorController.cs
+++ PierresTreats/Controllers/FlavorController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using PierresTreats.Models;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
